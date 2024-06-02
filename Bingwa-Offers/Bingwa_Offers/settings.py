6c9c4a468f39de08b135993daeacab15daae7ded
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-06-02 23:07:48 +0300

    Update settings.py

--- Bingwa-Offers/Bingwa_Offers/settings.py
+++ Bingwa-Offers/Bingwa_Offers/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apps.products',
+    'apps.orders',
+    'apps.payments',
+    'apps.notifications',
+    'home',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
